4ee073dda734d230461fdeca8636950f501ec804
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-06-04 15:26:59 +0200

    account for neighbors geohashes

--- api/index.go
+++ api/index.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Index struct {
-	density   int
+	limit     int
 	byGeohash map[uint64][]Indexable
 }
 
@@ -18,20 +18,24 @@ type Indexable interface {
 
 func NewIndex() *Index {
 	return &Index{
-		density:   100,
+		limit:     100,
 		byGeohash: map[uint64][]Indexable{},
 	}
 }
 
 func (index *Index) Insert(f Indexable) {
 	lon, lat := f.Coordinates()
-	h := geohash.EncodeIntWithPrecision(lat, lon, 16)
-	index.byGeohash[h] = append(index.byGeohash[h], f)
+	hash := geohash.EncodeIntWithPrecision(lat, lon, 16)
+	index.byGeohash[hash] = append(index.byGeohash[hash], f)
+
+	for _, nhash := range geohash.NeighborsIntWithPrecision(hash, 16) {
+		index.byGeohash[nhash] = append(index.byGeohash[nhash], f)
+	}
 }
 
 func (index *Index) Find(lon, lat float64) []Indexable {
-	h := geohash.EncodeIntWithPrecision(lat, lon, 16)
-	return index.byGeohash[h]
+	hash := geohash.EncodeIntWithPrecision(lat, lon, 16)
+	return index.byGeohash[hash]
 }
 
 func (index *Index) FindInBox(minLon, minLat, maxLon, maxLat float64) []Indexable {
@@ -47,11 +51,11 @@ func (index *Index) FindInBox(minLon, minLat, maxLon, maxLat float64) []Indexabl
 
 func (index *Index) Finalize() {
 	for i, slice := range index.byGeohash {
-		if len(slice) > index.density {
+		if len(slice) > index.limit {
 			sort.Slice(slice, func(a, b int) bool {
 				return slice[a].Score() > slice[b].Score()
 			})
-			index.byGeohash[i] = index.byGeohash[i][:index.density]
+			index.byGeohash[i] = index.byGeohash[i][:index.limit]
 		}
 	}
 }
